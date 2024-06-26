13a333938fa7fca05e4eeb6da0af8bbb9d197374
Victor Hugo Colquehuanca Flores <vhugocf@hotmail.com>
2024-06-25 21:49:12 -0300

    Initial Creation

--- Makefile
+++ Makefile
@@ -5,14 +5,13 @@
 setup: install-go init-go                                          ❷
 
 build:
-    go build -o api cmd/main.go
+	go build -o api cmd/main.go
  
 #TODO add MacOS support
  install-go:                                                        ❸
   wget "https://golang.org/dl/go$(GO_VERSION).linux-amd64.tar.gz"
-  sudo tar -C /usr/local -xzf go$(GO_VERSION).linux-amd64.tar.gz
-  rm go$(GO_VERSION).linux-amd64.tar.gz
- 
+	sudo tar -C /usr/local -xzf go$(GO_VERSION).linux-amd64.tar.gz
+	rm go$(GO_VERSION).linux-amd64.tar.gz
 init-go:                                                           ❹
   echo 'export PATH=$$PATH:/usr/local/go/bin' >> $${HOME}/.bashrc
   echo 'export PATH=$$PATH:$${HOME}/go/bin' >> $${HOME}/.bashrc
---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	//Hardcoding system port, for now, we will be able to configure it in the future
+	addr := ":8080"                                     
+ 
+    mux := http.NewServeMux()
+ 
+    mux.HandleFunc("/hello",
+      func(w http.ResponseWriter, r *http.Request){    
+        enc := json.NewEncoder(w)
+        w.Header().
+        Set("Content-Type",
+        "application/json; charset=utf-8")              
+        resp := Resp{
+            Language:    "English",
+            Translation: "Hello",
+        }
+        if err := enc.Encode(resp); err != nil {
+            panic("unable to encode response")
+        }
+    })
+
+	log.Printf("listening on %s\n", addr) 
+
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
+type Resp struct {
+	Language 	string `json:"language"`
+	Translation string `json:"translation"`
+}
